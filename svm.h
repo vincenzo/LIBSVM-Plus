579aeb7fb4d6abae221b429001a4d83c46f0a64a
Mikko Koppanen <mkoppanen@php.net>
2010-07-18 08:14:50 +0100

    Added visual studio templates

--- svm.h
+++ svm.h
@@ -3,6 +3,18 @@
 
 #define LIBSVM_VERSION 290
 
+#ifdef _WIN32
+
+#ifndef _WINDLL
+# define LIBSVM_EXPORT __declspec(dllimport)
+#else
+# define LIBSVM_EXPORT __declspec(dllexport)
+#endif
+
+#else
+# define LIBSVM_EXPORT 
+#endif /* WIN32 */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -129,28 +141,28 @@ struct decision_function
 };
 
 
-struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
-void svm_cross_validation(const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
+LIBSVM_EXPORT struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
+LIBSVM_EXPORT void svm_cross_validation(const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
 
-int svm_save_model(const char *model_file_name, const struct svm_model *model);
-struct svm_model *svm_load_model(const char *model_file_name);
+LIBSVM_EXPORT int svm_save_model(const char *model_file_name, const struct svm_model *model);
+LIBSVM_EXPORT struct svm_model *svm_load_model(const char *model_file_name);
 
-int svm_get_svm_type(const struct svm_model *model);
-int svm_get_nr_class(const struct svm_model *model);
-void svm_get_labels(const struct svm_model *model, int *label);
-double svm_get_svr_probability(const struct svm_model *model);
+LIBSVM_EXPORT int svm_get_svm_type(const struct svm_model *model);
+LIBSVM_EXPORT int svm_get_nr_class(const struct svm_model *model);
+LIBSVM_EXPORT void svm_get_labels(const struct svm_model *model, int *label);
+LIBSVM_EXPORT double svm_get_svr_probability(const struct svm_model *model);
 
-void svm_predict_values(const struct svm_model *model, const struct svm_node *x, double* dec_values);
-double svm_predict(const struct svm_model *model, const struct svm_node *x);
-double svm_predict_probability(const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
+LIBSVM_EXPORT void svm_predict_values(const struct svm_model *model, const struct svm_node *x, double* dec_values);
+LIBSVM_EXPORT double svm_predict(const struct svm_model *model, const struct svm_node *x);
+LIBSVM_EXPORT double svm_predict_probability(const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
 
-void svm_destroy_model(struct svm_model *model);
-void svm_destroy_param(struct svm_parameter *param);
+LIBSVM_EXPORT void svm_destroy_model(struct svm_model *model);
+LIBSVM_EXPORT void svm_destroy_param(struct svm_parameter *param);
 
-const char *svm_check_parameter(const struct svm_problem *prob, const struct svm_parameter *param);
-int svm_check_probability_model(const struct svm_model *model);
+LIBSVM_EXPORT const char *svm_check_parameter(const struct svm_problem *prob, const struct svm_parameter *param);
+LIBSVM_EXPORT int svm_check_probability_model(const struct svm_model *model);
 
-extern void (*svm_print_string) (const char *);
+LIBSVM_EXPORT extern void (*svm_print_string) (const char *);
 
 #ifdef __cplusplus
 }
